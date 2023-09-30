c0cc12373ec1c5c406e3649894f999e324d25204
ChristopherVsn <christopher.voisin1@gmail.com>
2023-09-30 14:43:20 +0200

    Add js on header

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//Header & nav bar
+const header = document.getElementById('header');
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+    let st = window.scrollY;
+    if (st > lastScrollTop) {
+        header.style.top = "-100px";
+    } else {
+        header.style.top = "0";
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+});
