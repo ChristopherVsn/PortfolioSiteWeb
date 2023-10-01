0c0a4b354fb0eb73e63cd746d9f649df128449e8
ChristopherVsn <christopher.voisin1@gmail.com>
2023-10-01 16:21:28 +0200

    Js | GenerateBubble - #AboutMe

--- js/index.js
+++ js/index.js
@@ -21,7 +21,6 @@ function isElementInViewport(e) {
     let rect = e.getBoundingClientRect();
     let elementHeight = rect.bottom - rect.top;
     let offset = elementHeight / 2;
-    console.log(rect.bottom,window.innerHeight);
     return (
         rect.bottom-offset <= (window.innerHeight) &&
         rect.top+offset >=0
@@ -43,3 +42,88 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+
+
+// Generate bubble on aboutMe BackGround
+let nbClicked=0;
+
+function createBubble() {
+    let rect= document.getElementById('aboutMe').getBoundingClientRect();
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
+    let topOrBot = Math.round(Math.random());
+    let xPos, yPos;
+
+    if (topOrBot==0){
+        xPos = Math.random() * window.innerWidth;
+        yPos = 0;
+    } else {
+        xPos = Math.random() * window.innerWidth;
+        yPos = 300;
+    }
+    bubble.style.left = xPos+"px";
+    bubble.style.top = yPos+"px";
+
+    const size = Math.random() * 50 + 15;
+    bubble.style.width = size+"px";
+    bubble.style.height = size+"px";
+
+    const color = getRandomColor();
+    bubble.style.backgroundColor = color;
+
+    animateBubble(bubble, topOrBot);
+    addOnClickBubble(bubble);
+    document.getElementById('aboutMe').appendChild(bubble);
+}
+
+
+function addOnClickBubble(bubble) {
+    bubble.addEventListener('click', () => {
+        bubble.remove();
+        nbClicked++;
+        if (nbClicked==50){
+            alert("Vous avez éclaté 50 bulles");
+        }
+    });
+}
+
+
+function animateBubble(bubble, topOrBot) {
+    let rect= document.getElementById('aboutMe').getBoundingClientRect();
+    let xDirection = (Math.random() - 0.5) * 2 * rect.width;
+    let yDirection;
+
+    if (topOrBot==0){
+        yDirection ="350px";
+    } else{
+        yDirection ="-100vh";
+    }
+
+    bubble.animate(
+        [
+            { transform: 'translate(0, 0)' },
+            { transform: `translate(${xDirection}px, ${yDirection})` },
+        ],
+        {
+            duration: Math.random() * 5000 + 5000,
+            iterations: 1,
+        }
+    ).onfinish = function () {
+        bubble.remove();
+        createBubble();
+    };
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+for (let i = 0; i < 6; i++) {
+    setTimeout(createBubble, i * 1500);
+}
