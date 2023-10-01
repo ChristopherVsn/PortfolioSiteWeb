30d5c70814717373a7d1a1a48e22c1db06a514b4
ChristopherVsn <christopher.voisin1@gmail.com>
2023-10-01 12:46:57 +0200

    add js on timeline

--- js/index.js
+++ js/index.js
@@ -11,3 +11,35 @@ window.addEventListener("scroll", () => {
     }
     lastScrollTop = st <= 0 ? 0 : st;
 }); 
+
+
+// TimeLine
+const allLeftBlock= document.querySelectorAll('.timeline-block-left')
+const allRightBlock= document.querySelectorAll('.timeline-block-right')
+
+function isElementInViewport(e) {
+    let rect = e.getBoundingClientRect();
+    let elementHeight = rect.bottom - rect.top;
+    let offset = elementHeight / 2;
+    console.log(rect.bottom,window.innerHeight);
+    return (
+        rect.bottom-offset <= (window.innerHeight) &&
+        rect.top+offset >=0
+    );
+}
+window.addEventListener('scroll', () => {
+    allLeftBlock.forEach(leftBlock => {
+        if (isElementInViewport(leftBlock)) {
+            leftBlock.classList.add('timeline-left-block-show');
+        } else{
+            leftBlock.classList.remove('timeline-left-block-show');
+        }
+    }); 
+    allRightBlock.forEach(rightBlock => {
+        if (isElementInViewport(rightBlock)) {
+            rightBlock.classList.add('timeline-right-block-show');
+        } else {
+            rightBlock.classList.remove('timeline-right-block-show');
+        }
+    });
+});
